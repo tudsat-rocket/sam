ae1f9e86ff59288b3e34641aa2faa50ada438781
Felix Wiegand <felix-wiegand@protonmail.ch>
2022-12-05 22:08:08 +0100

    Reduce flash SPI frequency
    Doesn't cause CPU utilization issues due to DMA, might cut down on
    errors

--- firmwares/flight_computer/src/main.rs
+++ firmwares/flight_computer/src/main.rs
@@ -170,7 +170,7 @@ fn main() -> ! {
     let spi2_cs_flash = gpiob.pb12.into_push_pull_output_in_state(PinState::High);
     let spi2 = dp
         .SPI2
-        .spi((spi2_sck, spi2_miso, spi2_mosi), spi_mode, 42.MHz(), &clocks);
+        .spi((spi2_sck, spi2_miso, spi2_mosi), spi_mode, 20.MHz(), &clocks);
     let flash_dma_streams = hal::dma::StreamsTuple::new(dp.DMA1);
     let flash = Flash::init(spi2, spi2_cs_flash, flash_dma_streams);
 
