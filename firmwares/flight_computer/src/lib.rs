dbe0f5414bb02b27d1914b955e21179217f34104
Felix Wiegand <felix-wiegand@protonmail.ch>
2023-01-04 01:14:19 +0100

    Only load embedded dependencies for no_std env

--- firmwares/flight_computer/src/lib.rs
+++ firmwares/flight_computer/src/lib.rs
@@ -8,22 +8,22 @@ mod params;
 pub mod telemetry;
 pub use params::*;
 
-#[cfg(not(feature = "std"))]
+#[cfg(feature = "no_std")]
 use defmt_rtt as _; // global logger (TODO)
 
-#[cfg(not(feature = "std"))]
+#[cfg(feature = "no_std")]
 use panic_probe as _;
 
 /// same panicking *behavior* as `panic-probe` but doesn't print a panic message
 /// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
 #[defmt::panic_handler]
-#[cfg(not(feature = "std"))]
+#[cfg(feature = "no_std")]
 fn panic() -> ! {
     cortex_m::asm::udf()
 }
 
 /// Terminates the application and makes `probe-run` exit with exit-code = 0
-#[cfg(not(feature = "std"))]
+#[cfg(feature = "no_std")]
 pub fn exit() -> ! {
     loop {
         cortex_m::asm::bkpt();
