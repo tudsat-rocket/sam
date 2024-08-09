63de7b034810026bd8b2689c7a723c736d3dfe21
Luis Wientgens <windgans@protonmail.com>
2024-08-09 17:43:23 +0200

    Add firmware skeleton

---
+++ firmwares/fancyfins/src/can.rs
@@ -0,0 +1,98 @@
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::can::{Can, CanRx, CanTx};
+use embassy_stm32::can::bxcan::{Frame, Id, StandardId};
+use embassy_stm32::peripherals::CAN;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+
+use embassy_sync::pubsub::{PubSubChannel, Publisher, Subscriber};
+use static_cell::StaticCell;
+
+pub const CAN_QUEUE_SIZE: usize = 5; // TODO
+pub const NUM_CAN_SUBSCRIBERS: usize = 3; // TODO
+pub const NUM_CAN_PUBLISHERS: usize = 5; // TODO
+
+pub type CanFrame = (u16, [u8; 8]);
+pub type CanInChannel = PubSubChannel::<CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, NUM_CAN_SUBSCRIBERS, 1>;
+pub type CanInSubscriper = Subscriber::<'static, CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, NUM_CAN_SUBSCRIBERS, 1>;
+pub type CanOutChannel = PubSubChannel::<CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, 1, NUM_CAN_PUBLISHERS>;
+pub type CanOutPublisher = Publisher::<'static, CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, 1, NUM_CAN_PUBLISHERS>;
+
+pub static CAN_IN: StaticCell<CanInChannel> = StaticCell::new();
+pub static CAN_OUT: StaticCell<CanOutChannel> = StaticCell::new();
+
+static CAN: StaticCell<Can<'static, CAN>> = StaticCell::new();
+static CAN_TX: StaticCell<CanTx<'static, 'static, CAN>> = StaticCell::new();
+static CAN_RX: StaticCell<CanRx<'static, 'static, CAN>> = StaticCell::new();
+
+pub async fn spawn(
+    mut can: Can<'static, CAN>,
+    spawner: Spawner,
+    publisher: Publisher<'static, CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, NUM_CAN_SUBSCRIBERS, 1>,
+    subscriber: Subscriber<'static, CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, 1, NUM_CAN_PUBLISHERS>,
+) {
+    can.modify_config()
+        .set_loopback(false)
+        .set_silent(false)
+        .leave_disabled();
+    can.set_bitrate(125_000);
+    can.enable().await;
+
+    let can = CAN.init(can);
+    let (can_tx, can_rx) = can.split();
+    let can_tx = CAN_TX.init(can_tx);
+    let can_rx = CAN_RX.init(can_rx);
+
+    spawner.spawn(run_tx(can_tx, subscriber)).unwrap();
+    spawner.spawn(run_rx(can_rx, publisher)).unwrap();
+}
+
+#[embassy_executor::task]
+async fn run_tx(
+    can_tx: &'static mut CanTx<'static, 'static, CAN>,
+    mut subscriber: Subscriber<'static, CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, 1, NUM_CAN_PUBLISHERS>,
+) -> ! {
+    loop {
+        let (address, data) = subscriber.next_message_pure().await;
+        let Some(sid) = StandardId::new(address) else {
+            continue;
+        };
+
+        let frame = Frame::new_data(sid, data);
+        can_tx.write(&frame).await;
+        can_tx.flush_all().await;
+    }
+}
+
+#[embassy_executor::task]
+async fn run_rx(
+    can_rx: &'static mut CanRx<'static, 'static, CAN>,
+    publisher: Publisher<'static, CriticalSectionRawMutex, CanFrame, CAN_QUEUE_SIZE, NUM_CAN_SUBSCRIBERS, 1>,
+) -> ! {
+    loop {
+        match can_rx.read().await {
+            Ok(envelope) => {
+                let frame = envelope.frame;
+                let Some(data) = frame.data() else {
+                    continue;
+                };
+
+                let Id::Standard(sid) = frame.id() else {
+                    // EID package, skipping.
+                    continue;
+                };
+                let id_raw = sid.as_raw();
+
+                let Ok(data_array) = data.as_ref().try_into() else {
+                    // frame wasn't 8 bytes long, skip.
+                    continue;
+                };
+
+                publisher.publish_immediate((id_raw, data_array));
+            }
+            Err(e) => {
+                error!("Failed to read envelope: {:?}", Debug2Format(&e))
+            }
+        }
+    }
+}
---
+++ firmwares/fancyfins/src/main.rs
@@ -0,0 +1,79 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::{InterruptExecutor, Spawner};
+use embassy_stm32::Config;
+use embassy_stm32::adc::Adc;
+use embassy_stm32::interrupt::{Priority, InterruptExt};
+use embassy_stm32::interrupt;
+use embassy_stm32::peripherals::*;
+use embassy_stm32::time::Hertz;
+use embassy_stm32::usart::Uart;
+use embassy_stm32::wdg::IndependentWatchdog;
+use embassy_sync::pubsub::PubSubChannel;
+use embassy_time::{Duration, Ticker, Timer};
+
+use {defmt_rtt as _, panic_probe as _};
+
+mod fin_tasks;
+mod can;
+
+use fin_tasks::*;
+use can::*;
+
+
+// TODO
+#[global_allocator]
+static ALLOCATOR: alloc_cortex_m::CortexMHeap = alloc_cortex_m::CortexMHeap::empty();
+
+static EXECUTOR_HIGH: InterruptExecutor = InterruptExecutor::new();
+
+embassy_stm32::bind_interrupts!(struct Irqs {
+    CAN1_RX1 => embassy_stm32::can::Rx1InterruptHandler<CAN>;
+    CAN1_SCE => embassy_stm32::can::SceInterruptHandler<CAN>;
+    USB_LP_CAN1_RX0 => embassy_stm32::can::Rx0InterruptHandler<CAN>;
+    USB_HP_CAN1_TX => embassy_stm32::can::TxInterruptHandler<CAN>;
+
+    USART3 => embassy_stm32::usart::InterruptHandler<USART3>;
+});
+
+#[embassy_executor::task]
+async fn iwdg_task(mut iwdg: IndependentWatchdog<'static, IWDG>) -> ! {
+    let mut ticker = Ticker::every(Duration::from_millis(256));
+    loop {
+        iwdg.pet();
+        ticker.next().await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(low_priority_spawner: Spawner) {
+    let mut config = Config::default();
+    config.rcc.hse = Some(Hertz::mhz(8));
+    config.rcc.sys_ck = Some(Hertz::mhz(72));
+    config.rcc.hclk = Some(Hertz::mhz(72));
+    config.rcc.pclk1 = Some(Hertz::mhz(36));
+    config.rcc.pclk2 = Some(Hertz::mhz(72));
+    config.rcc.adcclk = Some(Hertz::mhz(14));
+    let mut p = embassy_stm32::init(config);
+
+    defmt::info!("Fin startup");
+    
+    // Start watchdog
+    let mut iwdg = IndependentWatchdog::new(p.IWDG, 512_000); // 512ms timeout
+    iwdg.unleash();
+
+    // adc for strain gauges
+    let adc = Adc<'static, ADC1>;
+
+    let can_in = CAN_IN.init(PubSubChannel::new());
+    let can_out = CAN_OUT.init(PubSubChannel::new());
+    // Can RX on PB8 can TX on PB9
+    let mut can = embassy_stm32::can::Can::new(p.CAN, p.PB8, p.PB9, Irqs);
+
+    // startup can
+    can::spawn(can, low_priority_spawner, can_in.publisher().unwrap(), can_out.subscriber().unwrap()).await;
+   
+    low_priority_spawner.spawn(iwdg_task(iwdg)).unwrap();
+    
+}
