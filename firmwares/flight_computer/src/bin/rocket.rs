02fac65142f15a150d759672f6b6912bb362f1e0
Felix Wiegand <felix-wiegand@protonmail.ch>
2025-07-06 20:19:40 +0200

    Reenable high- and medium-priority executors

--- firmwares/flight_computer/src/bin/rocket.rs
+++ firmwares/flight_computer/src/bin/rocket.rs
@@ -50,16 +50,25 @@ static EXECUTOR_MEDIUM: InterruptExecutor = InterruptExecutor::new();
 async fn main(low_priority_spawner: Spawner) {
     let (mut board, settings, seed) = fw::init_board().await;
 
+    // Start high priority executor
+    interrupt::I2C3_EV.set_priority(Priority::P6);
+    let high_priority_spawner = EXECUTOR_HIGH.start(interrupt::I2C3_EV);
+
+    // Start medium priority executor
+    interrupt::I2C3_ER.set_priority(Priority::P7);
+    let medium_priority_spawner = EXECUTOR_MEDIUM.start(interrupt::I2C3_ER);
+
+    let lora_downlink =
+        fw::lora::start_rocket_downlink(board.lora1, settings.lora.clone(), medium_priority_spawner);
+    let lora_uplink =
+        fw::lora::start_rocket_uplink(board.lora2, settings.lora.clone(), medium_priority_spawner);
+
     let (can1_tx, can1_rx) = ((), ());
     let (can2_tx, can2_rx) = ((), ());
+
+    let (eth_downlink, eth_uplink) = fw::ethernet::start(board.ethernet, low_priority_spawner, seed);
     let (usb_downlink, usb_uplink) = ((), ());
     //let (usb_downlink, usb_uplink) = fw::usb::start(board.usb, low_priority_spawner);
-    let (eth_downlink, eth_uplink) = fw::ethernet::start(board.ethernet, low_priority_spawner, seed);
-    // TODO: medium priority
-    let lora_downlink =
-        fw::lora::start_rocket_downlink(board.lora1, settings.lora.clone(), low_priority_spawner.make_send());
-    let lora_uplink =
-        fw::lora::start_rocket_uplink(board.lora2, settings.lora.clone(), low_priority_spawner.make_send());
 
     let vehicle = Vehicle::init(
         board.sensors,
@@ -72,36 +81,28 @@ async fn main(low_priority_spawner: Spawner) {
         settings,
     );
 
-    //// Start high priority executor
-    //interrupt::I2C3_EV.set_priority(Priority::P6);
-    //let high_priority_spawner = EXECUTOR_HIGH.start(interrupt::I2C3_EV);
-
-    //// Start medium priority executor
-    //interrupt::I2C3_ER.set_priority(Priority::P7);
-    //let medium_priority_spawner = EXECUTOR_MEDIUM.start(interrupt::I2C3_ER);
-
     board.iwdg.unleash();
 
-    //high_priority_spawner.spawn(vehicle::run(vehicle, iwdg)).unwrap();
-    low_priority_spawner.spawn(flight_computer_firmware::vehicle::run(vehicle, board.iwdg)).unwrap();
+    high_priority_spawner.spawn(fw::vehicle::run(vehicle, board.iwdg)).unwrap();
 
     //medium_priority_spawner.spawn(can::run_tx(can_tx)).unwrap();
     //medium_priority_spawner.spawn(can::run_rx(can_rx)).unwrap();
     //medium_priority_spawner.spawn(drivers::sensors::gps::run(gps)).unwrap(); // TODO: priority?
     //medium_priority_spawner.spawn(flash::run(flash)).unwrap();
 
+    low_priority_spawner.spawn(fw::buzzer::run(board.buzzer.0, board.buzzer.1)).unwrap();
     low_priority_spawner.spawn(guard_task()).unwrap();
 }
 
-//#[interrupt]
-//unsafe fn I2C3_EV() {
-//    EXECUTOR_HIGH.on_interrupt()
-//}
-//
-//#[interrupt]
-//unsafe fn I2C3_ER() {
-//    EXECUTOR_MEDIUM.on_interrupt()
-//}
+#[interrupt]
+unsafe fn I2C3_EV() {
+    EXECUTOR_HIGH.on_interrupt()
+}
+
+#[interrupt]
+unsafe fn I2C3_ER() {
+    EXECUTOR_MEDIUM.on_interrupt()
+}
 
 #[embassy_executor::task]
 pub async fn guard_task() -> ! {
