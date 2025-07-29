1abb2ac9f6d6f1c17aac74a0a4b9c8def6f01fcc
Jonas Bug <jonas.bug@protonmail.com>
2025-07-29 22:00:29 +0200

    fix(firmwares/engine_board): switch servo control to connector 1

--- firmwares/engine_board/src/actors.rs
+++ firmwares/engine_board/src/actors.rs
@@ -5,17 +5,18 @@ use embassy_time::{Duration, Ticker};
 #[embassy_executor::task]
 pub async fn run_servo_check(mut pwm: SimplePwm<'static, TIM3>, toggle_time: Duration) -> ! {
     let mut ticker = Ticker::every(toggle_time);
-    let mut pwm_out = pwm.ch3();
+    let mut pwm_out = pwm.ch4();
     pwm_out.enable();
 
     loop {
-        // 1 ms high (in 20ms period)
+        // 50Hz = 20ms period
         pwm_out.set_duty_cycle_percent(5);
+        defmt::info!("duty_cycle low");
 
         ticker.next().await;
 
-        // 2 ms high (in 20ms period)
-        pwm_out.set_duty_cycle_percent(10);
+        pwm_out.set_duty_cycle_percent(8);
+        defmt::info!("duty_cycle high");
 
         ticker.next().await;
     }
